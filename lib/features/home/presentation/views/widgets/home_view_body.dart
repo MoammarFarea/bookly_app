8f8937e260c73694ad0eb9f76eb9c322d18a07c9
Moammar Farea <moammarfarea@gmail.com>
2025-10-19 20:14:03 +0300

    refactor HomeViewBody: extract FeaturedBooksListView widget and update layout

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -7,6 +7,28 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), const CustomListViewItem()]);
+    return Column(
+      children: [const CustomAppBar(), const FeaturedBooksListView()],
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: CustomListViewItem(),
+          );
+        },
+      ),
+    );
   }
 }
