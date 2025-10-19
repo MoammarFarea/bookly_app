10e189e74694f6d72947c44e597919dca45c544f
Moammar Farea <moammarfarea@gmail.com>
2025-10-19 19:59:39 +0300

    add CustomListViewItem widget and update HomeViewBody to include it; add test image asset

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          // height: MediaQuery.of(context).size.height * .4,
+          // width: MediaQuery.of(context).size.width * .58,
+          decoration: BoxDecoration(
+            // color: Colors.red,
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
